c563eb2c65d52745b9f64968490ce286f803ca95
Mike Panitz <michpa@hotmail.com>
2023-05-14 09:44:12 -0700

    Initial Commit

---
+++ Chapter15/Chapter15App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter15App.swift
+//  Chapter15
+//
+//  Created by Mike Panitz on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Chapter15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
